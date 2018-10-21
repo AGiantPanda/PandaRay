6d7bb8a848e23339c5031a5a688c62af94c9901f
PaaandaChen <pandachen0611@cmlab.csie.ntu.edu.tw>
2018-10-21 12:43:58 +0800

    hello world of my PandaRay

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    std::ofstream output;
+    output.open("output.ppm");
+
+    int nx = 200;
+    int ny = 100;
+    output << "P3\n" << nx << " " << ny << "\n255\n";
+    for (int j = ny-1; j>=0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float r = float(i) / float(nx);
+            float g = float(j) / float(ny);
+            float b = 0.2;
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+            output << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+
+    output.close();
+    return 0;
+}

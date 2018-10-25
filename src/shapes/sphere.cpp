4ef914298d182903a4356c3d682aac3e3755db08
Panda <pandachen0611@gmail.com>
2018-10-25 17:35:36 +0800

    add episilon when comparing to zero

--- src/shapes/sphere.cpp
+++ src/shapes/sphere.cpp
@@ -14,21 +14,22 @@ Sphere::Sphere(float p0, float p1, float p2, float rr) {
 
 bool Sphere::Intersect(const Ray &ray, float &tHit) const {
     Vec3f co = ray.Origin() - center;
+    const float episilon = 1e-5;
     float a = dot(ray.Direction(), ray.Direction());
     float b = 2.0 * dot(ray.Direction(), co);
     float c = dot(co, co) - radius * radius;
     float discriminant = b*b - 4*a*c;
 
-    if (discriminant < 0) {
+    if (discriminant < 0.0 -  episilon) {
         return false;
     } else {
         float min = (-b - sqrt(discriminant)) / (2.0 * a);
         float max = (-b + sqrt(discriminant)) / (2.0 * a);
 
-        if (min >= 0.0) {
+        if (min >= 0.0 - episilon) {
             tHit = min;
             return true;
-        } else if (max >= 0.0) {
+        } else if (max >= 0.0 - episilon) {
             tHit = max;
             return true;
         } else {

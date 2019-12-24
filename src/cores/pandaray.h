82a5fedc6aff3258893e7cd1739f55e209860f87
PaaandaChen <pandachen0611@gmail.com>
2019-12-24 11:28:27 +0800

    Revert "include rearrange"
    This reverts commit 158afa2d47c9030a4d16ff227006ac87c389e9e2.

--- src/cores/pandaray.h
+++ src/cores/pandaray.h
@@ -12,7 +12,6 @@
 #include <functional>
 #include <random>
 #include <ctime>
-#include <thread>
 
 inline double random_double()
 {
@@ -31,15 +30,15 @@ static float Inv2Pi = 0.15915494309189533577;
 
 #include "../cores/vec3.h"
 #include "../cores/ray.h"
-#include "../cores/camera.h"
 
-#include "../cores/shape.h"
-#include "../shapes/shapelist.h"
-#include "../shapes/sphere.h"
+// #include "../cores/shape.h"
+// #include "../cores/material.h"
 
-#include "../cores/material.h"
-#include "../materials/lambertian.h"
-#include "../materials/metal.h"
-#include "../materials/dielectric.h"
+// Global Forward Declaration
+class Shape;
+class Material;
+struct hit_record;
+
+
 
 #endif
